e05c6f02ad9c1f1040e5ac2c0a67db3bbea87d4b
c_tran <trancongchuan2018@gmail.com>
2025-01-02 21:24:30 +0700

    main

--- js/script.js
+++ js/script.js
@@ -1,2 +1,48 @@
-const ber = 'v1'
-console.log(ber)
+const swiper = new Swiper('.swiperSlider', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
+const swiper2 = new Swiper(".catSwiper", {
+  slidesPerView: 5,
+  spaceBetween: 16,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiper3 = new Swiper(".blogSwiper", {
+  slidesPerView: 5,
+  spaceBetween: 16,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiper4 = new Swiper(".doitacSwiper", {
+  slidesPerView: 7,
+  spaceBetween: 16,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
